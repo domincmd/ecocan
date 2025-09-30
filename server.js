c173d732eb434817031f2afb55b499f7f9a50f4d
domincmd <felipe.caliento.domingueti@gmail.com>
2025-09-29 17:12:03 -0700

    pls make cloud work 4

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 
-const adapter = new FileSync('data.json')
+const adapter = new FileSync('tmp/data.json')
 const db = low(adapter)
 
 
